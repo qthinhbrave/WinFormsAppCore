de6ab746c8bcd3aa58a14c71b26364fcdbd15ee1
DESKTOP-LGKS0NQ\QThinh <QThinh@BSV-THINHNGUYEN>
2023-05-30 13:09:33 +0700

    Add project files.

---
+++ WinFormsAppCore/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WinFormsAppCore
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            button1 = new Button();
+            checkBox1 = new CheckBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(41, 28);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(594, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(41, 78);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(693, 184);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 10);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Input :";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(41, 55);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(678, 30);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(56, 19);
+            checkBox1.TabIndex = 4;
+            checkBox1.Text = "Suffix";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(checkBox1);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private Button button1;
+        private CheckBox checkBox1;
+    }
+}
